3ad8bf4286331c8bd595e218cf725af713c68928
Nicholas Poggioli <shopping@cerlify.com>
2024-11-01 15:46:57 -0400

    Create 164-code.R

---
+++ 164-code.R
@@ -0,0 +1,6 @@
+# 164 Business and Biodiversity chapter for SAGE Handbook of Business in Society
+# This file contains code for bibliometric anlaysis of related publications.
+# Author: Nicholas A. Poggioli (poggiolin@appstate.edu)
+# Date:   November 1, 2024
+
+
