a613b59da8deac9766140ce22fde8c90ad24176e
Nicholas Poggioli <shopping@cerlify.com>
2024-11-04 16:32:12 -0500

    Update 164-code.R

--- 164-code.R
+++ 164-code.R
@@ -1,6 +1,6 @@
 #### 164 Business and Biodiversity chapter for SAGE Handbook of Business in Society ####
 # This file contains code for bibliometric anlaysis of related publications using the bibliometrix package https://www.bibliometrix.org/home/index.php/download.
-# Author: Nicholas A. Poggioli (poggiolin@appstate.edu)
+# Author: Nicholas A. Poggioli
 # Date:   November 1, 2024
 
 # Install bibliometrix package
@@ -10,5 +10,5 @@
 library(bibliometrix)
 
 # Import data from Scopus search (https://www.bibliometrix.org/vignettes/Data-Importing-and-Converting.html)
-file <- "https://github.com/nicholaspoggioli/164-business-biodiversity-chapter/blob/main/20241101-scopus-export-all.csv"
+file <- "https://github.com/nicholaspoggioli/164-business-biodiversity-chapter/blob/main/20241101-scopus-export-clean.csv"
 M <- convert2df(file, dbsource = "scopus", format = "csv")
