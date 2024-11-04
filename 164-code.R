4409b536b6e4e04eb2a98ef7792937d4cd62429d
Nicholas Poggioli <shopping@cerlify.com>
2024-11-04 16:50:47 -0500

    Create clean csv to change column heading names

--- 164-code.R
+++ 164-code.R
@@ -10,5 +10,5 @@
 library(bibliometrix)
 
 # Import data from Scopus search (https://www.bibliometrix.org/vignettes/Data-Importing-and-Converting.html)
-file <- "https://github.com/nicholaspoggioli/164-business-biodiversity-chapter/blob/main/20241101-scopus-export-all.csv"
+file <- "https://github.com/nicholaspoggioli/164-business-biodiversity-chapter/blob/main/20241101-scopus-export-all-clean.csv"
 M <- convert2df(file, dbsource = "scopus", format = "csv")
